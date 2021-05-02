f12447dff4e269a0789d36297e49ffe6178f67cd
jjcastle1992 <77635923+jjcastle1992@users.noreply.github.com>
2021-05-02 13:37:38 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*********************
+Name: James Castle
+COSC 2436 Final Project
+Purpose: Implementation for my driver file that will be testing my airport/route graph object.
+**********************/
+
+#include "main.h"
+
+int main () {
+
+    
+    return 0;
+};
---
+++ main.h
@@ -0,0 +1,15 @@
+/*********************
+Name: James Castle
+COSC 2436 Final Project
+Purpose: Interface for my driver file that will be testing my airport/route graph object.
+**********************/
+
+#ifndef CODE_ASSIGN9_MAIN_H
+#define CODE_ASSIGN9_MAIN_H
+
+#include <iostream>
+#include <string>
+
+#include "graph.h"
+
+#endif //CODE_ASSIGN9_MAIN_H
