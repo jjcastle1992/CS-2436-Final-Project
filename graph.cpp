954a26f6e1bafdd808cfcb880db4afd4ebf70f33
jjcastle1992 <77635923+jjcastle1992@users.noreply.github.com>
2021-05-02 13:57:04 -0500

    setup basic structure for method definitions in graph implementation file.

--- graph.cpp
+++ graph.cpp
@@ -5,3 +5,43 @@ Purpose: Implementation file for my graph object that will contain an object tha
 **********************/
 
 #include "graph.h"
+//Constructor
+Graph::Graph () {
+
+};
+//Destructor
+Graph::~Graph() {
+
+}
+//Getters
+int Graph::numberAirports() {
+
+}
+int Graph::numberRoutes () {
+
+}
+string Graph::findAirport (int) {
+
+}
+
+//Vertex add/removal
+bool Graph::addAirport (int, string) {
+
+}
+bool Graph::removeAirport (int) {
+
+}
+void Graph::clearAirports () {
+
+}
+
+//Printing/Traversal methods
+void Graph::displayGraph () {
+
+}
+void Graph::depthFirstSearch() {
+
+}
+void Graph::breadthFirstSearch() {
+    
+}
