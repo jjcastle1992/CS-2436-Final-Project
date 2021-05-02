f990e6ce27650bdfb0f88464cb22064964ac27bd
jjcastle1992 <77635923+jjcastle1992@users.noreply.github.com>
2021-05-02 15:52:09 -0500

    Added parallel arrays for test data in main

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ Purpose: Implementation for my driver file that will be testing my airport/route
 int main () {
 
     Graph airportNetwork;
+    int tempIDs [] = {0, 1, 2, 3, 4, 5};
+    string tempCodes [] = {"AUS","DFW", "SFO", "LGA", "SEA", "LAX"};
+    int idCodeCount = 0;
 
     std::cout << "Testing display function on a graph object without any airports..." << std::endl;
     std::cout << "--------------------------" <<std::endl;
@@ -18,20 +21,22 @@ int main () {
 
     std::cout << "\nTesting simple addition of a single airport to our vector..." << std::endl;
     std::cout << "--------------------------" <<std::endl;
-    airportNetwork.addAirport(1, "DFW");
+    airportNetwork.addAirport(tempIDs[idCodeCount], tempCodes[idCodeCount++]);
     airportNetwork.displayGraph();
     std::cout << "Number of airports: " << airportNetwork.numberAirports() << std::endl;
     std::cout << "Number of routes: " << airportNetwork.numberRoutes() << std::endl;
 
     std::cout << "\nTesting  addition of a second airport (precursor to adding first route)..." << std::endl;
     std::cout << "--------------------------" <<std::endl;
-    airportNetwork.addAirport(2, "AUS");
+    airportNetwork.addAirport(tempIDs[idCodeCount], tempCodes[idCodeCount++]);
     airportNetwork.displayGraph();
     std::cout << "Number of airports: " << airportNetwork.numberAirports() << std::endl;
     std::cout << "Number of routes: " << airportNetwork.numberRoutes() << std::endl;
 
     std::cout << "\nTesting  addition of a first route..." << std::endl;
     std::cout << "--------------------------" <<std::endl;
+
+    //Note adding airports 3-5 can be done via a loop since we've proven proof of concept on AUS and DFW.
 
     return 0;
 };
