57c78f81b7f986e844f628361abc143aae9b7484
jjcastle1992 <77635923+jjcastle1992@users.noreply.github.com>
2021-05-09 15:23:28 -0500

    Added basic functionalites based on Exam 1 queue modified for this application.

--- queue.h
+++ queue.h
@@ -6,5 +6,30 @@ Purpose: Interface file for my queue object that be used for BFS as the "positio
 
 #ifndef CODE_ASSIGN9_QUEUE_H
 #define CODE_ASSIGN9_QUEUE_H
+#include <iostream>
+
+class Queue {
+
+public:
+
+    Queue(int);  // constructor
+    ~Queue();  // destructor
+
+    bool pull(int*);
+    bool peek(int*);
+    bool push(int);
+    bool isEmpty();
+
+    // this is a programmer debug routine
+    // DO NOT LEAVE THIS IN PRODUCTION CODE
+    void dumpStack();
+
+private:
+    int size; // so we can pass and set this value when doing BFS.
+    int back;  //do not initialize here, that's what the constructor is for
+    int front;
+    int queue[];
+
+};
 
 #endif //CODE_ASSIGN9_QUEUE_H
